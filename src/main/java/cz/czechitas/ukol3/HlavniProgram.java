7a9545822db95f83d91661aa9d6976a3705898aa
iMarketa <marketa.kleger@gmail.com>
2022-05-10 22:59:42 +0200

    Procesor

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -6,8 +6,57 @@ package cz.czechitas.ukol3;
 public class HlavniProgram {
 
     public static void main(String... args) {
-        //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
-        System.out.println("Program spuštěn.");
+
+        System.out.println("Informace o iMarketa PC:");
+        Pocitac imarketaPocitac;
+        imarketaPocitac = new Pocitac();
+        System.out.println(imarketaPocitac);
+        imarketaPocitac.zapniSe();
+
+        //"Počítač startuje, vyčkej do úplného zapnutí."
+        //"Počítač nemůže nastartovat, zkontrolujte jednotlivé komponenty např. paměť a oba disky."
+
+        Procesor imarketaProcesor = new Procesor();
+        imarketaProcesor.setRychlost(3_400_000_000_00L);
+        imarketaProcesor.setVyrobce("Dell");
+
+        Pamet imarketaPamet = new Pamet();
+        imarketaPamet.setKapacita(16_000_000_000L);
+
+        Disk imarketaDisk1 = new Disk();
+        imarketaDisk1.setKapacitaDisku(255_195_746_304L);
+        Disk imarketaDisk2 = new Disk();
+        imarketaDisk2.setKapacitaDisku(255_195_746_304L);
+
+        imarketaPocitac.setCpu(imarketaProcesor);
+        imarketaPocitac.setRam(imarketaPamet);
+        imarketaPocitac.setPevnyDisk1(imarketaDisk1);
+        imarketaPocitac.setDisk2(imarketaDisk2);
+
+        System.out.println(imarketaDisk1);
+        System.out.println(imarketaDisk2);
+
+        System.out.println(imarketaPocitac);
+
+        imarketaPocitac.zapniSe();
+        //pc běží, vypíše se chyba "Počítač je již zapnutý."
+
+        imarketaPocitac.zapniSe();
+        System.out.println(imarketaPocitac);
+
+        imarketaPocitac.vytvorSouborOVelikosti2Disku(255_195_746_304L);
+        imarketaPocitac.vytvorSouborOVelikosti2Disku(255_195_746_304L - 100_000_000L);
+        System.out.println(imarketaPocitac);
+        imarketaPocitac.vymazSouborOVelikosti2Disku(100_000_000L);
+        imarketaPocitac.vytvorSouborOVelikosti2Disku(100_000_001L);
+        imarketaPocitac.vymazSouborOVelikosti2Disku(255_195_746_304L - 100_000_000_000L);
+        imarketaPocitac.vymazSouborOVelikosti2Disku(255_195_746_304L - 100_000_000L);
+        imarketaPocitac.vymazSouborOVelikosti2Disku(255_195_746_304L + 200_000_000L);
+        System.out.println(imarketaPocitac);
+
+        imarketaPocitac.vypniSe();
+        imarketaPocitac.vypniSe();
+        imarketaPocitac.vytvorSouborOVelikosti(300000);
     }
 
 }
